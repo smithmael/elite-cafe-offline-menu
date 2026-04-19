873f83e65509be31c0736f99769980b34ba1325a
smithmael <smithmiz91@gmail.com>
2026-04-19 05:59:15 -0300

    deploy on santiy for firm and vercel to website

--- studio/sanity.cli.ts
+++ studio/sanity.cli.ts
@@ -1,15 +1,12 @@
 import {defineCliConfig} from 'sanity/cli'
 
+// studio/sanity.cli.ts
 export default defineCliConfig({
   api: {
     projectId: 'q4duhjks',
     dataset: 'production'
   },
   deployment: {
-    /**
-     * Enable auto-updates for studios.
-     * Learn more at https://www.sanity.io/docs/studio/latest-version-of-sanity#k47faf43faf56
-     */
-    autoUpdates: true,
+    appId: 'smgwxxz1wfm3pvwrdh3lg8p7', // Paste it here
   }
 })
